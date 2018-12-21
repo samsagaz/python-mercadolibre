1b52413d8f1bb9b64a098297398bcf9566136e75
Eduardo Enriquez (eduzen) <eduardo.a.enriquez@gmail.com>
2018-12-21 20:39:11 -0300

    working on profiles

--- python_mercadolibre/users/models.py
+++ python_mercadolibre/users/models.py
@@ -1,9 +1,27 @@
+from collections import namedtuple
+
+Address = namedtuple('Address', ["address", "city", "state", "zip_code"])
+Phone = namedtuple("Phone", ["area_code", "extension", "number", "verified"])
+
+
 class Profile:
     def __init__(self, **kwargs):
+        self.raw_data = dict(**kwargs)
         self.__dict__.update(kwargs)
+        if hasattr(self, 'address'):
+            self.address = Address(**self.address)
+        if hasattr(self, 'phone'):
+            self.phone = Phone(**self.phone)
+
+    def __eq__(self, other):
+        if isinstance(other, Profile):
+            return self.__dict__ == other.__dict__
+        else:
+            return NotImplemented
 
     def __repr__(self):
-        return f"<User(nickname={self.nickname!r})>"
+        if hasattr(self, 'nickname'):
+            cls_name = self.__class__.__name__
+            return '<{} {!r}>'.format(cls_name, self.nickname)
 
-    def __str__(self):
-        return f"<User(nickname={self.nickname})>"
+        return super().__repr__()
