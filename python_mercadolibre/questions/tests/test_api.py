22677adde312cdbadebff7ca3c506ca9021ea6f4
Jose Luis <joseluiszanotti@gmail.com>
2019-01-05 16:21:47 -0300

    Fixture added to test

--- python_mercadolibre/questions/tests/test_api.py
+++ python_mercadolibre/questions/tests/test_api.py
@@ -3,17 +3,33 @@ from python_mercadolibre.questions.models import QuestionModel
 from python_mercadolibre.questions.api import Question
 from python_mercadolibre.users.api import User
 
-user = User()
-profile = user.profile()
-question = Question()
+
+@pytest.fixture
+@pytest.mark.vcr()
+def user():
+    return User()
 
 
-def test_by_seller_inputs():
+@pytest.fixture
+@pytest.mark.vcr()
+def profile(user):
+    return user.profile()
+
+
+@pytest.fixture
+@pytest.mark.vcr()
+def question():
+    return Question()
+
+
+@pytest.mark.vcr()
+def test_by_seller_inputs(question, profile):
     questions_by_seller = question.by_seller(profile.id)
     assert isinstance(questions_by_seller, QuestionModel)
 
 
-@pytest.mark.parametrize("input", [profile, user, str, [], {}, (), bool])
-def test_invalid_by_seller_inputs(input):
+@pytest.mark.vcr()
+@pytest.mark.parametrize("test_input", [profile, user, str, [], {}, (), bool])
+def test_invalid_inputs_by_seller(test_input):
     with pytest.raises(Exception):
-        question.by_seller(input)
+        question.by_seller(test_input)
